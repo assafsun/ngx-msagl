03c1ace39c4aede17883c6f890d70ba1b0cbbcb9
Assaf Shemesh <assaf.sun@gmail.com>
2021-08-14 12:00:24 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app-root/app.component';
+import { AppBarComponent } from './components/app-bar/app-bar/app-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
