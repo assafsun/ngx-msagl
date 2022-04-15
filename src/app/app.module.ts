a928419a2e08025726f3d54f09a56b8a46c2dca6
Assaf Shemesh <assaf.sun@gmail.com>
2022-04-15 16:15:47 +0300

    Add ngx-msagl component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app-root/app.component';
 import { AppBarComponent } from './components/app-bar/app-bar/app-bar.component';
+import { NgxGraphMSAGLComponent } from 'src/ngx-msagl-src/ngx-graph-msagl/ngx-graph-msagl.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppBarComponent
+    AppBarComponent,
+    NgxGraphMSAGLComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
