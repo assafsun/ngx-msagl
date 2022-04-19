fee080d0c4620953dca36487f7e6e85bb12f8bad
Assaf Shemesh <assaf.sun@gmail.com>
2022-04-19 17:51:04 +0300

    Integration with dot files - part 1

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ngxMsaglEdge, ngxMsaglNode } from 'src/ngx-msagl-src/models';
+import * as GraphlibDot from 'graphlib-dot';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-  }
+  public nodes: ngxMsaglNode[] = [];
+  public links: ngxMsaglEdge[] = [];
+  public shouldShowGraph:boolean = false;
 
+  async ngOnInit(): Promise<void> {
+    const resp = await fetch("https://raw.githubusercontent.com/microsoft/msagljs/main/modules/core/test/data/graphvis/world.gv");
+    const content = await resp.text();
+    const graph = GraphlibDot.read(content);
+    this.nodes = graph.nodes().map((node) => {return {id: node, label:"L"+node}});
+    this.links = graph.edges().map((edge) => {return {source: edge.v, target: edge.w}});
+    this.shouldShowGraph = true;
+  }
 }
